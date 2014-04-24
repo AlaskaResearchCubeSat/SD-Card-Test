d560f015649f035d156dce6f13881ea995620707
Jesse Frey <jmfrey@alaska.edu>
2014-04-24 10:28:20 -0800

    Changed to use commands from commandLib and added mmccount command

--- SDtst_errors.h
+++ SDtst_errors.h
@@ -1,10 +1,9 @@
 #ifndef __SDTST_ERRORS_H
   #define __SDTST_ERRORS_H
+  #include <commandLib.h>
   
   //error sources for BUS test program
-  enum{SDTST_ERR_SRC_CMD=ERR_SRC_SUBSYSTEM};
+  //enum{=ERR_SRC_CMD+1};
     
-  //command errors
-  enum{CMD_ERR_RESET};
 #endif
   
